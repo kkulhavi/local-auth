9c3a418f5d4100b5b6097af92dc6dce47d6d1ef0
kkulhavi <78230187+kkulhavi@users.noreply.github.com>
2022-06-05 11:07:34 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': 'us-cdbr-east-05.cleardb.net',
+        'user': 'b489bbeb692707',
+        'password': '2093ff03'
     },
 	'database': 'my_schema',
     'users_table': 'users'
